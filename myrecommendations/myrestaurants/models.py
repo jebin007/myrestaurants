d3d054c8721df83751c94db05e3699eb81f7b3f3
jebin007 <jevinsubedi@gmail.com>
2017-09-30 04:24:59 -0500

    Made Restaurant Model Class

--- myrecommendations/myrestaurants/models.py
+++ myrecommendations/myrestaurants/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from datetime import date
 # Create your models here.
+
+class Restaurant(models.Model):
+    name = models.TextField()
+    street = models.TextField(blank=True, null=True)
+    number = models.IntegerField(blank=True, null=True)
+    city = models.TextField(default="")
+    zipCode = models.TextField(blank=True, null=True)
+    stateOrProvince = models.TextField(blank=True, null=True)
+    country = models.TextField(blank=True, null=True)
+    telephone = models. TextField(blank=True, null=True)
+    url = models.URLField(blank=True, null=True)
+    user = models.ForeignKey(User, default=1)
+    date = models.DateField(default=date.today)
+
+    def __str__(self):
+        return self.name
