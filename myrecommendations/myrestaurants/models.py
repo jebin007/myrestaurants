bf963623a82bed554466a0014de93c728c66a670
jebin007 <jevinsubedi@gmail.com>
2017-09-30 04:38:16 -0500

    Made  Review Model Class

--- myrecommendations/myrestaurants/models.py
+++ myrecommendations/myrestaurants/models.py
@@ -31,3 +31,13 @@ class Dish(models.Model):
 
     def __str__(self):
         return self.name
+
+class Review(models.Model):
+    RATING_CHOICES = ((1, 'one'), (2, 'two'), (3, 'three'), (4, 'four'), (5, 'five'))
+    rating = models.PositiveSmallIntegerField('Rating (stars)', blank=False, default=3, choices=RATING_CHOICES)
+    comment = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(User, default=1)
+    date = models.DateField(default=date.today)
+
+    class Meta:
+        abstract = True
