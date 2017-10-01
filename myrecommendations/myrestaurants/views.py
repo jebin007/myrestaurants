d84c73429b144e35767caa411b5de041e298263e
jebin007 <jevinsubedi@gmail.com>
2017-09-30 21:12:42 -0500

    Defined RestaurantDetail Class

--- myrecommendations/myrestaurants/views.py
+++ myrecommendations/myrestaurants/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView, CreateView
+from . models import RestaurantReview, Restaurant, Dish
+from forms import RestaurantForm, DishForm
 
 # Create your views here.
+
+class RestaurantDetail(DetailView):
+    model = Restaurant
+    template_name = 'myrestaurants/restaurant_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(RestaurantDetail, self).get_context_data(**kwargs)
+        context['RATING_CHOICES'] = RestaurantReview.RATING_CHOICES
+        return context
